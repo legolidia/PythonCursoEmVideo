d564d9d38a0f86c8d97ea1d05e789c0e66ae312a
legolidia <32281374@mackenzista.com.br>
2022-10-10 18:16:35 -0300

    mudança de var

--- d03.py
+++ d03.py
@@ -1,5 +1,5 @@
-n1, n2 = int(input()), int(input())
+n1, n4 = int(input()), int(input())
 
-soma = n1+n2
+soma = n1+n4
 
-print("{}+{}={}".format(n1, n2, soma))
+print("{}+{}={}".format(n1, n4, soma))
